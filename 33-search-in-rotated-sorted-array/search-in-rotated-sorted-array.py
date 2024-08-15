c13b67854aa905fd6df7d1c652b0af4fca87f841
Yajas Arora <yajasarora22030@gmail.com>
2024-08-15 18:52:12 +0530

    Time: 22 ms (63.31%) | Memory: 11.9 MB (63.25%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         l,r=0,len(nums)-1
         while l<=r:
-            m=r-l//2
+            m=l+(r-l)//2
             if nums[m]==target:
                 return m
             elif nums[l]<=nums[m]:
