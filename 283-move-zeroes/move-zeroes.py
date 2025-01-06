b4f6d1be10327f38f7c75211ed56903bd01c1314
Yajas Arora <yajasarora22030@gmail.com>
2025-01-06 18:10:23 +0530

    Time: 3 ms (90.40%) | Memory: 13.5 MB (23.68%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -7,7 +7,5 @@ class Solution(object):
         i=0
         for j in range(len(nums)):
             if nums[j]!=0:
-                nums[i]=nums[j]
-                if i!=j:
-                    nums[j]=0
+                nums[i],nums[j]=nums[j],nums[i]
                 i+=1
