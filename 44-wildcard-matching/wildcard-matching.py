26b818bea57da6e3d3974dee9fc05ba94566dfd6
Yajas Arora <yajasarora22030@gmail.com>
2025-01-17 13:02:50 +0530

    Time: 534 ms (69.94%) | Memory: 20.5 MB (52.71%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -3,29 +3,46 @@ class Solution(object):
     
         n = len(pattern)
         m = len(text)
-        mem = {}
+        dp=[[False]*(m+1) for _ in range(n+1)]
 
-        def f(n, m):
-            if (n, m) in mem:
-                return mem[(n, m)]
+        dp[0][0]=True
+        for i in range(1,n+1):
+            if pattern[i-1]=='*':
+                dp[i][0]=dp[i-1][0]
 
-            # Base cases
-            if n == 0 and m == 0:
-                mem[(n, m)] = True
-            elif n == 0:
-                mem[(n, m)] = False
-            elif m == 0:
-                mem[(n, m)] = all(i == '*' for i in pattern[:n])
-            else:
-                # If characters match or '?' matches any character
-                if pattern[n - 1] == text[m - 1] or pattern[n - 1] == '?':
-                    mem[(n, m)] = f(n - 1, m - 1)
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if pattern[i-1] == text[j-1] or pattern[i-1] == '?':
+                    dp[i][j] = dp[i-1][j-1]
                 # If '*' matches zero or more characters
-                elif pattern[n - 1] == '*':
-                    mem[(n, m)] = f(n - 1, m) or f(n, m - 1)
+                elif pattern[i-1] == '*':
+                    dp[i][j] = dp[i-1][j] or dp[i][j-1]
                 else:
-                    mem[(n, m)] = False
+                    dp[i][j] = False
+        return dp[n][m]
+
+
+        # def f(n, m):
+        #     if (n, m) in mem:
+        #         return mem[(n, m)]
+
+        #     # Base cases
+        #     if n == 0 and m == 0:
+        #         mem[(n, m)] = True
+        #     elif n == 0:
+        #         mem[(n, m)] = False
+        #     elif m == 0:
+        #         mem[(n, m)] = all(i == '*' for i in pattern[:n])
+        #     else:
+        #         # If characters match or '?' matches any character
+        #         if pattern[n - 1] == text[m - 1] or pattern[n - 1] == '?':
+        #             mem[(n, m)] = f(n - 1, m - 1)
+        #         # If '*' matches zero or more characters
+        #         elif pattern[n - 1] == '*':
+        #             mem[(n, m)] = f(n - 1, m) or f(n, m - 1)
+        #         else:
+        #             mem[(n, m)] = False
             
-            return mem[(n, m)]
-        return f(n,m)
+        #     return mem[(n, m)]
+        # return f(n,m)
 
